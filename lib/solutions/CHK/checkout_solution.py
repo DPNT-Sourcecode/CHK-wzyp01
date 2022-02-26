db87d12359cd1ae40f7b740cac396f400e41a2b0
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 11:35:09 +0000

    Sat Feb 26 11:35:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,17 +5,41 @@
 def checkout(skus):
     empty_string = skus.replace("A", "").replace("B", "").replace("C", "").replace("D", "")
     if len(empty_string) > 0:
-        raise ValueError("Non valid characters in SKU")
+        return -1
 
     As = skus.count("A")
     Bs = skus.count("B")
     Cs = skus.count("C")
     Ds = skus.count("D")
-    print(As)
+
+    price_A = 50
+    price_B = 30
+    price_C = 20
+    price_D = 15
+
+    three_As_price = 130
+    two_Bs_price = 45
+
+    ret = 0
+    while As >= 3:
+        ret += three_As_price
+        As -= 3
+
+    while Bs >= 2:
+        ret += two_Bs_price
+        Bs -= 2
+
+    ret += As*price_A
+    ret += Bs*price_B
+    ret += Cs*price_C
+    ret += Ds*price_D
+
+    return ret
 
 
 if __name__ == "__main__":
-    checkout("AAA")
-    checkout("a")
+    print(checkout("AAA"))
+    print(checkout("AABB"))
+
 
 
