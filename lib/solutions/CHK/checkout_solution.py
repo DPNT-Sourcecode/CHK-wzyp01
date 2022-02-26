3d793e3ac003e093ebae70104a4a2aa397d54187
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 11:32:09 +0000

    Sat Feb 26 11:32:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,10 +3,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    empty_string = skus.replace("A", "").replace("B", "").replace("C", "").replace("D", "")
+    if len(empty_string) > 0:
+        raise ValueError("Non valid characters in SKU")
+
     As = skus.count("A")
+    Bs = skus.count("B")
+    Cs = skus.count("C")
+    Ds = skus.count("D")
     print(As)
 
 
 if __name__ == "__main__":
     checkout("AAA")
+    checkout("a")
+
 
