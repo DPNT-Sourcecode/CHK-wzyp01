047bc0ba49cf89113d2400b47402d6fb608b231c
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 12:18:09 +0000

    Sat Feb 26 12:18:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,77 +1,88 @@
 
-
+prices = {"A": 50,
+          "B": 30,
+          "C": 20,
+          "D": 15,
+          "E": 40,
+          "F": 10,
+          "G": 20,
+          "H": 10,
+          "I": 35,
+          "J": 60,
+          "K": 80,
+          "L": 90,
+          "M": 15,
+          "N": 40,
+          "O": 10,
+          "P": 50,
+          "Q": 30,
+          "R": 50,
+          "S": 30,
+          "T": 20,
+          "U": 40,
+          "V": 50,
+          "W": 20,
+          "X": 90,
+          "Y": 10,
+          "Z": 50}
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     if not isinstance(skus, str):
         return -1
 
-    empty_string = skus\
-        .replace("A", "")\
-        .replace("B", "")\
-        .replace("C", "")\
-        .replace("D", "")\
-        .replace("E", "")\
-        .replace("F", "")
+    empty_string = skus
+
+    for sku in prices:
+        empty_string = empty_string.replace(sku, "")
 
     if len(empty_string) > 0:
         return -1
 
-    As = skus.count("A")
-    Bs = skus.count("B")
-    Cs = skus.count("C")
-    Ds = skus.count("D")
-    Es = skus.count("E")
-    Fs = skus.count("F")
+    sku_counts = dict()
 
-    price_A = 50
-    price_B = 30
-    price_C = 20
-    price_D = 15
-    price_E = 40
-    price_F = 10
+    for sku in prices:
+        sku_counts[sku] = skus.count(sku)
 
     five_As_price = 200
     three_As_price = 130
     two_Bs_price = 45
-    three_Fs_price = 2*price_F
+    three_Fs_price = 2*prices["F"]
 
-    temp_Es = Es
+    temp_Es = sku_counts["E"]
     while temp_Es >= 2:
         temp_Es -= 2
-        if Bs > 0:
-            Bs -= 1
+        if sku_counts["B"] > 0:
+            sku_counts["B"] -= 1
 
     ret = 0
 
-    while As >= 5:
+    while sku_counts["A"] >= 5:
         ret += five_As_price
-        As -= 5
+        sku_counts["A"] -= 5
 
-    while As >= 3:
+    while sku_counts["A"] >= 3:
         ret += three_As_price
-        As -= 3
+        sku_counts["A"] -= 3
 
-    while Bs >= 2:
+    while sku_counts["B"] >= 2:
         ret += two_Bs_price
-        Bs -= 2
+        sku_counts["B"] -= 2
 
-    while Fs >= 3:
+    while sku_counts["F"] >= 3:
         ret += three_Fs_price
-        Fs -= 3
+        sku_counts["F"] -= 3
 
-    ret += As*price_A
-    ret += Bs*price_B
-    ret += Cs*price_C
-    ret += Ds*price_D
-    ret += Es*price_E
-    ret += Fs*price_F
+    for sku in sku_counts:
+        ret += sku_counts[sku] * prices[sku]
 
     return ret
 
 
 if __name__ == "__main__":
+    print(checkout("A"))
     print(checkout("AAA"))
     print(checkout("AABB"))
     print(checkout("EEBB"))
     print(checkout("AAAAA"))
+
