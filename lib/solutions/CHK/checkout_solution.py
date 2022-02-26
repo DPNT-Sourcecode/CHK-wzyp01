03cb4e3f7b42db5004ef08a882252803f7a44a17
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 11:49:44 +0000

    Sat Feb 26 11:49:44 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,7 +6,13 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
 
-    empty_string = skus.replace("A", "").replace("B", "").replace("C", "").replace("D", "")
+    empty_string = skus\
+        .replace("A", "")\
+        .replace("B", "")\
+        .replace("C", "")\
+        .replace("D", "")\
+        .replace("E", "")
+
     if len(empty_string) > 0:
         return -1
 
@@ -14,16 +20,30 @@ def checkout(skus):
     Bs = skus.count("B")
     Cs = skus.count("C")
     Ds = skus.count("D")
+    Es = skus.count("E")
 
     price_A = 50
     price_B = 30
     price_C = 20
     price_D = 15
+    price_E = 40
 
+    five_As_price = 200
     three_As_price = 130
     two_Bs_price = 45
 
+    temp_Es = Es
+    while temp_Es >= 2:
+        temp_Es -= 2
+        if Bs > 0:
+            Bs -= 1
+            
     ret = 0
+
+    while As >= 5:
+        ret += five_As_price
+        As -= 5
+
     while As >= 3:
         ret += three_As_price
         As -= 3
@@ -36,6 +56,7 @@ def checkout(skus):
     ret += Bs*price_B
     ret += Cs*price_C
     ret += Ds*price_D
+    ret += Es*price_E
 
     return ret
 
@@ -43,6 +64,4 @@ def checkout(skus):
 if __name__ == "__main__":
     print(checkout("AAA"))
     print(checkout("AABB"))
-
-
-
+    print(checkout("EEBB"))
