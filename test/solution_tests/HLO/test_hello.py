69c3bfadb2fa1c0b1f56983e911e9fc4c68ff3d0
sbx_user1051 <sbx_user1051@169.254.4.77>
2022-02-26 20:22:05 +0000

    Sat Feb 26 20:22:05 UTC 2022

--- test/solution_tests/HLO/test_hello.py
+++
@@ -1,9 +0,0 @@
-from solutions.HLO import hello_solution
-import pytest
-
-class TestHello():
-    def test_hello(self):
-        assert hello_solution.hello("abc") == "Hello, abc!"
-
-        with pytest.raises(TypeError):
-            hello_solution.hello(1)
