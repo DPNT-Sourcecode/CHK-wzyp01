52f9686084f516659fc4098d2bd49343c983a1f1
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 12:30:09 +0000

    Sat Feb 26 12:30:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -31,6 +31,7 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
 
+    # check valid input by removing all valid skus and checking if string is length 0
     empty_string = skus
 
     for sku in prices:
@@ -39,16 +40,14 @@ def checkout(skus):
     if len(empty_string) > 0:
         return -1
 
+    # fill the number of each sku into the dict
     sku_counts = dict()
-
     for sku in prices:
         sku_counts[sku] = skus.count(sku)
 
-    five_As_price = 200
-    three_As_price = 130
-    two_Bs_price = 45
-    three_Fs_price = 2*prices["F"]
+    # special logic for the promotions
 
+    # buy one type get another type free
     temp_Es = sku_counts["E"]
     while temp_Es >= 2:
         temp_Es -= 2
@@ -57,22 +56,44 @@ def checkout(skus):
 
     ret = 0
 
-    while sku_counts["A"] >= 5:
-        ret += five_As_price
-        sku_counts["A"] -= 5
+    # multipack prices
+    multi_pack_prices = dict()
+    # dict of letter, then number, then price of that number
+    # need to have highest number first
+    multi_pack_prices["A"]= ((5, 3), (200, 130))
+    multi_pack_prices["B"] = ((2), (45))
+    multi_pack_prices["F"] = [(3), (2 * prices["F"])]
 
-    while sku_counts["A"] >= 3:
-        ret += three_As_price
-        sku_counts["A"] -= 3
+    for sku in sku_counts:
+        if sku in multi_pack_prices:
+            print(sku, multi_pack_prices[sku])
+            for (n, multi_pack_price) in multi_pack_prices[sku]:
+                while sku_counts[sku] >= n:
+                    ret += multi_pack_price
+                    sku_counts[sku] -= n
+    # five_As_price = 200
+    # three_As_price = 130
+    # two_Bs_price = 45
+    # three_Fs_price = 2 * prices["F"]
+    #
+    # while sku_counts["A"] >= 5:
+    #     ret += five_As_price
+    #     sku_counts["A"] -= 5
+    #
+    # while sku_counts["A"] >= 3:
+    #     ret += three_As_price
+    #     sku_counts["A"] -= 3
+    #
+    # while sku_counts["B"] >= 2:
+    #     ret += two_Bs_price
+    #     sku_counts["B"] -= 2
+    #
+    # while sku_counts["F"] >= 3:
+    #     ret += three_Fs_price
+    #     sku_counts["F"] -= 3
+    #
 
-    while sku_counts["B"] >= 2:
-        ret += two_Bs_price
-        sku_counts["B"] -= 2
-
-    while sku_counts["F"] >= 3:
-        ret += three_Fs_price
-        sku_counts["F"] -= 3
-
+    # tally up the individual prices
     for sku in sku_counts:
         ret += sku_counts[sku] * prices[sku]
 
@@ -85,4 +106,5 @@ if __name__ == "__main__":
     print(checkout("AABB"))
     print(checkout("EEBB"))
     print(checkout("AAAAA"))
+
 
