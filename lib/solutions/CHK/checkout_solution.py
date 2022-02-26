73345af2e260b73c7418a933c808b553baa12e21
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 11:29:09 +0000

    Sat Feb 26 11:29:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,10 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    As = skus.count("A")
+    print(As)
+
+
+if __name__ == "__main__":
+    checkout("AAA")
+
