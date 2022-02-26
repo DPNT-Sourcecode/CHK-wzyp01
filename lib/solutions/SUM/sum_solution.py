99d2725f0edecc4ec072ed8bb03225b17e638ca6
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 11:09:53 +0000

    Sat Feb 26 11:09:53 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,9 +1,5 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    if x < 0 or x > 100:
-        raise ValueError("Value x cannot be negative or greater than 100")
-    if y < 0 or y > 100:
-        raise ValueError("Value y cannot be negative or greater than 100")
 
     if not isinstance(x, int):
         raise TypeError("x must be an integer")
@@ -11,8 +7,13 @@ def compute(x, y):
     if not isinstance(y, int):
         raise TypeError("y must be an integer")
 
+    if x < 0 or x > 100:
+        raise ValueError("Value x cannot be negative or greater than 100")
+    if y < 0 or y > 100:
+        raise ValueError("Value y cannot be negative or greater than 100")
 
     ret = x + y
     return ret
+
 
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,31 @@
 from solutions.SUM import sum_solution
-
+import pytest
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(-1, 1)
+
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(1, -1)
+
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(101, 1)
+
+        with pytest.raises(ValueError):
+            assert sum_solution.compute(1, 101)
+
+        with pytest.raises(TypeError):
+            assert sum_solution.compute(1.1, 2)
+
+        with pytest.raises(TypeError):
+            assert sum_solution.compute(2, 1.1)
+
+        with pytest.raises(TypeError):
+            assert sum_solution.compute("2", 1)
+
+        with pytest.raises(TypeError):
+            assert sum_solution.compute(1, "2")
+
