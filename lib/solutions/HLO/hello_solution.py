c2cc38ae90634fbff71116958d492b372403297c
sbx_user1051 <sbx_user1051@169.254.47.53>
2022-02-26 11:22:47 +0000

    Sat Feb 26 11:22:47 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,5 +3,13 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return "Hello, World!"
+    if not isinstance(friend_name, str):
+        raise TypeError("friend_name must be an integer")
+
+    return "Hello, {}!".format(friend_name)
+
+if __name__ == "__main__":
+    print(hello("John"))
+    print(hello(["john"]))
+
 
